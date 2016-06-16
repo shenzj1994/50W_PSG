e57e47223c9970efc71ff390a5d57f6f62048b86
Zhongjie Shen <shenzj1994@gmail.com>
2016-06-16 12:51:02 -0400

    -

--- app/src/main/java/com/canadiansolar/a50wportablesolargenerator/Web.java
+++ app/src/main/java/com/canadiansolar/a50wportablesolargenerator/Web.java
@@ -2,6 +2,7 @@ package com.canadiansolar.a50wportablesolargenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -12,8 +13,10 @@ public class Web extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Manual");
         setContentView(R.layout.activity_web);
         WebView wv=(WebView)findViewById(R.id.webView);
+
         WebSettings webSettings = wv.getSettings();
         webSettings.setJavaScriptEnabled(true);
         wv.setWebViewClient(new WebViewClient());
