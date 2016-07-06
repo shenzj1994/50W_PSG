1915f75a8dbc8ccefe964b8a155689692aa95208
Zhongjie Shen <shenzj1994@gmail.com>
2016-07-06 10:03:27 -0400

    Read from SPP and Log.d all the valid data.

--- app/src/main/java/com/canadiansolar/a50wportablesolargenerator/BT.java
+++ app/src/main/java/com/canadiansolar/a50wportablesolargenerator/BT.java
@@ -6,9 +6,12 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Size;
 import android.view.View;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
 
@@ -16,18 +19,22 @@ import java.util.UUID;
 public class BT extends Activity {
     public UUID MY_UUID;
 
-    Thread workerThread;
+    Thread connectThread;
+    Thread manageConnectedThread;
 
     private Set<BluetoothDevice> BTDevices;
     private BluetoothAdapter mBluetoothAdapter;
-    private BluetoothDevice mmDevice;
+
+    BluetoothSocket mmSocket;
+    InputStream mmInStream;
+    OutputStream mmOutStream;
+    BluetoothDevice mmDevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bt);
     }
-
 
     @Override
     protected void onStart() {
@@ -39,7 +46,6 @@ public class BT extends Activity {
         Log.d("UUID", MY_UUID.toString());
     }
 
-
     public void Connect_HC(View view) {
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         //Go through all the paired device and find the matched one. Then set the matched device as connection target
@@ -50,26 +56,33 @@ public class BT extends Activity {
                 break;
             }
         }
-
         //Start Worker Thread since the connecting process is a block call.
-        workerThread = new ConnectThread(mmDevice);
-        workerThread.start();
+        connectThread = new ConnectThread(mmDevice);
+        connectThread.start();
     }
 
-    public void onDestroy(View view) {
-        super.onDestroy();
+    public void SendLB(View view) {
+        try {
+            mmOutStream.write("\n\r".getBytes());
+            Log.d("Serial Write", "Write Successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private class ConnectThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final BluetoothDevice mmDevice;
+    public void Disconnect(View view) throws IOException {
+        mmSocket.close();
+    }
+
+    public class ConnectThread extends Thread {
+
 
         public ConnectThread(BluetoothDevice device) {
             // Use a temporary object that is later assigned to mmSocket,
             // because mmSocket is final
             BluetoothSocket tmp = null;
             mmDevice = device;
-            Log.d("Thread", "Start Thread");
+            Log.d("C_Thread", "Connecting Thread has Started");
 
             // Get a BluetoothSocket to connect with the given BluetoothDevice
             try {
@@ -83,14 +96,12 @@ public class BT extends Activity {
         public void run() {
             // Cancel discovery because it will slow down the connection
             mBluetoothAdapter.cancelDiscovery();
-            Log.d("Thread", "Start Run");
-
             try {
                 // Connect the device through the socket. This will block
                 // until it succeeds or throws an exception
-                Log.d("Thread", "Start Connect");
+                Log.d("C_Thread", "Connecting");
                 mmSocket.connect();
-                Log.d("Thread", "Tried Connect");
+                Log.d("C_Thread", "Connected Successfully!");
             } catch (IOException connectException) {
                 // Unable to connect; close the socket and get out
                 try {
@@ -99,6 +110,83 @@ public class BT extends Activity {
                 }
             }
             // Do work to manage the connection (in a separate thread)
+            manageConnectedThread = new manageConnectedThread(mmSocket);
+            manageConnectedThread.start();
         }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    public class manageConnectedThread extends Thread {
+        public manageConnectedThread(BluetoothSocket socket) {
+            Log.d("M_Thread", "Manage Thread has Started");
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+
+            //Always waiting for data
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    mmInStream.read(buffer);
+
+                    //Read out first 16 bytes in the buffer, in Hex.Only valid data which starts from 01 will be kept.
+                    for (int index = 0; index < 16; index++) {
+                        if (buffer[0] != 1) {
+                            break;
+                        }
+                        String hex = Integer.toHexString(buffer[index] & 0xFF);
+                        if (hex.length() == 1) {
+                            hex = '0' + hex;
+                        }
+                        Log.d("RECEIVED", hex);
+                    }
+
+                } catch (IOException e) {
+                    Log.d("E", "IO");
+                    break;
+                }
+            }
+        }
+
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+            }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+            }
+        }
+
+
     }
 }
